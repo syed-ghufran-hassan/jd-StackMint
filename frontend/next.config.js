ce8ea23509f5d6e5109ded2d6bfd5ff98a05eb42
jd <doej06456@gmail.com>
2026-01-26 23:06:18 +0100

    feat: add image domains and disable poweredByHeader in next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
+/**
+ * Next.js Configuration
+ * @type {import('next').NextConfig}
+ * @version 2.1.0
+ */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    domains: ['ipfs.io', 'gateway.pinata.cloud'],
+  },
 }
 
 module.exports = nextConfig
