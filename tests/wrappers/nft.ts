39f83544c098740d9d93fdee19f55a042ea6e444
jd <doej06456@gmail.com>
2026-01-30 11:06:10 +0100

    test: expand NFT wrapper with all contract functions

--- tests/wrappers/nft.ts
+++ tests/wrappers/nft.ts
@@ -23,6 +23,15 @@ export class StacksmintNft {
     );
   }
 
+  static batchMint(uris: string[], sender: string) {
+    return Tx.contractCall(
+      this.getContractName(),
+      "batch-mint",
+      [types.list(uris.map(uri => types.ascii(uri)))],
+      sender
+    );
+  }
+
   static transfer(tokenId: number, sender: string, recipient: string) {
     return Tx.contractCall(
       this.getContractName(),
@@ -37,6 +46,75 @@ export class StacksmintNft {
       this.getContractName(),
       "burn",
       [types.uint(tokenId)],
+      sender
+    );
+  }
+
+  static approve(spender: string, tokenId: number, sender: string) {
+    return Tx.contractCall(
+      this.getContractName(),
+      "approve",
+      [types.principal(spender), types.uint(tokenId)],
+      sender
+    );
+  }
+
+  static setApprovalForAll(operator: string, approved: boolean, sender: string) {
+    return Tx.contractCall(
+      this.getContractName(),
+      "set-approval-for-all",
+      [types.principal(operator), types.bool(approved)],
+      sender
+    );
+  }
+
+  static freezeToken(tokenId: number, sender: string) {
+    return Tx.contractCall(
+      this.getContractName(),
+      "freeze-token",
+      [types.uint(tokenId)],
+      sender
+    );
+  }
+
+  static unfreezeToken(tokenId: number, sender: string) {
+    return Tx.contractCall(
+      this.getContractName(),
+      "unfreeze-token",
+      [types.uint(tokenId)],
+      sender
+    );
+  }
+
+  static addDelegate(delegate: string, canMint: boolean, canTransfer: boolean, canList: boolean, duration: number, sender: string) {
+    return Tx.contractCall(
+      this.getContractName(),
+      "add-delegate",
+      [
+        types.principal(delegate),
+        types.bool(canMint),
+        types.bool(canTransfer),
+        types.bool(canList),
+        types.uint(duration)
+      ],
+      sender
+    );
+  }
+
+  static removeDelegate(delegate: string, sender: string) {
+    return Tx.contractCall(
+      this.getContractName(),
+      "remove-delegate",
+      [types.principal(delegate)],
+      sender
+    );
+  }
+
+  static setMintingPaused(paused: boolean, sender: string) {
+    return Tx.contractCall(
+      this.getContractName(),
+      "set-minting-paused",
+      [types.bool(paused)],
       sender
     );
   }
